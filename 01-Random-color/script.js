1f025b2438a9590dfbc5ffa8fa4371a59ddd93ae
CiceroSimplicio <ciceroandrade95@gmail.com>
2021-05-09 16:16:45 -0300

    concluido

---
+++ 01-Random-color/script.js
@@ -0,0 +1,17 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+const hexBtn = document.getElementById('hex-btn');
+const value = document.getElementById('value');
+
+// hex color
+hexBtn.addEventListener('click', () => {
+    let hexColor = '#';
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[randomHexNumber()];
+    }
+    value.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+})
+
+function randomHexNumber() {
+    return Math.floor(Math.random() * hex.length)
+}
